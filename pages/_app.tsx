17880a199ad63dd08323f0c784d18d8f4c5f027c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-04-26 16:24:46 -0600

    Wrap app in Editor Context Wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { EditorWrapper } from '../context/EditorContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <EditorWrapper>
+      <Component {...pageProps} />
+    </EditorWrapper>
+    )
+
 }
 
 export default MyApp
