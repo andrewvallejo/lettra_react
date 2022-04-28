8123d41f1523c6b7affdefcd76d2d087b916f489
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-04-28 14:29:47 -0600

    Move files into src folder

--- components/Nav.tsx
+++
@@ -1,5 +0,0 @@
-import styles from '../styles/nav.module.scss'
-
-export default function Nav ({children}: {children: any}){
-	return <div className={styles.nav}>{children}</div>
-}
--- components/logo.tsx
+++
@@ -1,3 +0,0 @@
-import Image from 'next/image'
-
-export const Logo = () => <Image src='/images/logo.png' height={85} width={85} alt='Lettra Logo' />
