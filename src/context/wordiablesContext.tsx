0fe4c62114781ac5b981ba17a1f5249798c3c52b
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-04-28 14:38:36 -0600

    Remove comments

--- src/context/wordiablesContext.tsx
+++ src/context/wordiablesContext.tsx
@@ -11,8 +11,6 @@ const WordiablesContext = createContext<IWordiables>(wordiables);
 export function WordiablesWrapper ({children}: WordiablesContextProviderProps){
 	const [wordiablesState, setWordiablesState] = useState<IWordiables>(wordiables);
 
-	// Any is currently being used to type contextValue,
-	// but you should try to look for a better solution
 	const contextValue: any = useMemo(
 		() => {
 			return [wordiablesState, setWordiablesState];
@@ -20,7 +18,6 @@ export function WordiablesWrapper ({children}: WordiablesContextProviderProps){
 		[wordiablesState, setWordiablesState]
 	);
 
-	// TS BUG: both items in array are throwing off tslinter (temp fix on line 16 w/ 'any')
 	return <WordiablesContext.Provider value={contextValue}>{children}</WordiablesContext.Provider>;
 }
 
