592f9923aa491b4f8188c7785da100bf606dc258
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-04-28 14:30:38 -0600

    Add file for editor utility functions

---
+++ src/lib/editor.tsx
@@ -0,0 +1,5 @@
+// This will run every time a new word is added to state in order to detect any words changes
+export const validateWordIndices = () => {};
+
+// If there are any changes in the word state then this will run to update all of the words to the correct indices
+export const updateWordIndices = () => {};
