591445864053e2d0e945449ba3eb5d82b27ab4ef
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-04-29 12:45:48 -0600

    Add IWord typing to functions return value

--- src/lib/editor.tsx
+++ src/lib/editor.tsx
@@ -1,13 +1,15 @@
+import { IWord } from "../../types";
+
 // This will run every time a new word is added to state in order to detect any words changes
 
 // If there are any changes in the word state then this will run to update all of the words to the correct indices
 
-export const updateWordIndices = (value: string): object[] => {
+export const updateWordIndices = (value: string): IWord[] => {
 	const words = value.trim().split(" ").filter((word) => word.length);
-	const wordiables: object[] = [];
+	const wordiables: IWord[] = [];
 
 	words.forEach((word, index) => {
-		const newWord = {
+		const newWord: IWord = {
 			string: word,
 			index: index,
 			tagged: false
