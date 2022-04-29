d3431a623c48f57fc599da26cf9c300787fe0136
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-04-29 12:45:22 -0600

    Add key values properties to numbers

--- src/context/wordiables.tsx
+++ src/context/wordiables.tsx
@@ -1,9 +1,37 @@
 export const wordiables = {
-	one: {},
-	two: {},
-	three: {},
-	four: {},
-	five: {},
-	six: {},
-	seven: {}
+	one: {
+		string: "",
+		index: 0,
+		tagged: false
+	},
+	two: {
+		string: "",
+		index: 0,
+		tagged: false
+	},
+	three: {
+		string: "",
+		index: 0,
+		tagged: false
+	},
+	four: {
+		string: "",
+		index: 0,
+		tagged: false
+	},
+	five: {
+		string: "",
+		index: 0,
+		tagged: false
+	},
+	six: {
+		string: "",
+		index: 0,
+		tagged: false
+	},
+	seven: {
+		string: "",
+		index: 0,
+		tagged: false
+	}
 };
