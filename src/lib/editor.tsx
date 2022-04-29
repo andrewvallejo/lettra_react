0c789573d93c02348ea68678a3fc04fb311ff9d0
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-04-28 18:49:16 -0600

    Add fn that turns words in objs and checks for '\'

--- src/lib/editor.tsx
+++ src/lib/editor.tsx
@@ -1,5 +1,28 @@
 // This will run every time a new word is added to state in order to detect any words changes
-export const validateWordIndices = () => {};
 
 // If there are any changes in the word state then this will run to update all of the words to the correct indices
-export const updateWordIndices = () => {};
+
+export const updateWordIndices = (value: string): object[] => {
+	const words = value.trim().split(" ").filter((word) => word.length);
+	const wordiables: object[] = [];
+
+	words.forEach((word, index) => {
+		const newWord = {
+			string: word,
+			index: index,
+			tagged: false
+		};
+
+		checkForSlash(newWord);
+		wordiables.push(newWord);
+	});
+
+	return wordiables;
+};
+
+const checkForSlash = (word: any) => {
+	if (word.string.includes("\\")) {
+		word.tagged = true;
+	}
+	return word;
+};
