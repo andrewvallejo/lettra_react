ba318ec92a287c15a3fe1cd2fd172938a010fb62
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-02 15:42:58 -0600

    Remove unnecessary component code

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,42 +1,13 @@
 import { SetStateAction, useEffect, useRef, useState } from "react";
 
 import styles from "../../styles/editor.module.scss";
-import { IWord } from "../../types";
 import { UseWordiablesContext } from "../context/wordiablesContext";
-import { stringToElement, updateWordIndices } from "../lib/editor";
+import LiveEditor from "./LiveEditor";
 
+// import { stringToElement } from "../lib/editor";
 export default function Editor (){
 	const [ wordiablesState, setWordiablesState ] = Object.values(UseWordiablesContext());
-	const [ words, setWords ] = useState<IWord[]>([]);
-	const [ textUI, setTextUI ] = useState<string>("");
-	const { wordiables } = wordiablesState;
-
-	const editorRef = useRef<HTMLTextAreaElement>(null);
-
-	useEffect(
-		() => {
-			!textUI && (setTextUI(""), setWords([]));
-		},
-		[ textUI ]
-	);
-
-	const handleChange = (event: { target: { value: SetStateAction<string> } }) => {
-		const { value } = event.target as HTMLTextAreaElement;
-		const lastChar = value[value.length - 1];
-		const charMarkers = [ ".", "?", "!", ",", ";", ":", " " ];
-
-		if (charMarkers.includes(lastChar)) {
-			setWords(updateWordIndices(value, wordiables.length));
-			// Need to first iterate through words to find tagged words.
-			words.forEach((word) => {
-				if (word.isWordiable && !wordiablesState.wordiables.includes(word.string)) {
-					setWordiablesState({ ...wordiablesState, wordiables: [ ...wordiables, word.string ] });
-				}
-			});
-		}
-
-		setTextUI(value);
-	};
+	const [ text, setText ] = useState("");
 
 	return (
 		<section className={styles.container}>
@@ -45,17 +16,15 @@ export default function Editor (){
 			</header>
 			<div className={styles.editor}>
 				<textarea
-					ref={editorRef}
-					value={textUI}
 					id='textEditor'
 					name='textEditor'
+					value={text}
 					className={styles.textEditor}
 					placeholder='Paste your cover letter here'
-					onChange={handleChange}
+					onChange={(e) => setText(e.target.value)}
 				/>
-
-				<div className={styles.liveEditor}>{stringToElement(words, wordiables)}</div>
 			</div>
+			<LiveEditor>{text}</LiveEditor>
 		</section>
 	);
 }
