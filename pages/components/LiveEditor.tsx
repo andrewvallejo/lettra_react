9f7fa919342416d47d396bfceb0ea97f9d0fec54
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-02 17:44:30 -0600

    Add text class to paragraph ele

--- pages/components/LiveEditor.tsx
+++ pages/components/LiveEditor.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from "react";
+
 import styles from "../../styles/editor.module.scss";
 
 export default function LiveEditor ({ children }: { children: any }){
 	return (
 		<div className={styles.liveEditor}>
-			<p>{children}</p>
+			<p className={styles.text}>{children}</p>
 		</div>
 	);
 }
