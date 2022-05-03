9b3ebd6c1db308a2afdf90dc2141d5ccde62fb49
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-03 14:03:55 -0600

    Add useEffects to update wordiables

--- components/LiveEditor.tsx
+++ components/LiveEditor.tsx
@@ -1,11 +1,25 @@
 import { useEffect, useState } from "react";
 
 import { UseWordiablesContext } from "../context/wordiablesContext";
-import { parseText } from "../lib/editor";
+import { parseText, searchForWordiables } from "../lib/editor";
 import styles from "../styles/editor.module.scss";
 
-export default function LiveEditor ({ children }: { children: any }){
-	const [ wordiablesState, setWordiablesState ] = Object.values(UseWordiablesContext());
+export default function LiveEditor ({ text }: { text: any }){
+	const { wordiables, setWordiables } = UseWordiablesContext();
 
-	return <div className={styles.liveEditor}>{parseText(children)}</div>;
+	useEffect(
+		() => {
+			if (text) {
+				text.split(" ").forEach((word: string) => {
+					const isWordiable: boolean = searchForWordiables(word);
+					if (isWordiable && !wordiables.includes(word)) {
+						setWordiables([ ...wordiables, word ]);
+					}
+				});
+			}
+		},
+		[ text, setWordiables, wordiables ]
+	);
+
+	return <div className={styles.liveEditor}>{parseText(text)}</div>;
 }
