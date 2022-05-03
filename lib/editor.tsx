06c71c1502846899fee935f62a92683314e7e41a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-02 18:35:51 -0600

    Split fn to ...concerns

--- lib/editor.tsx
+++ lib/editor.tsx
@@ -2,8 +2,12 @@ import styles from "../styles/editor.module.scss";
 
 export const parseText = (text: string) => {
 	const words = text.split(" ");
-	const phrase = words.reduce((phrase, word) => {
+	return convertStringToHTML(words);
+};
+
+const convertStringToHTML = (words: string[]) => {
+	const text = words.reduce((phrase, word) => {
 		return `${phrase} ${word}`;
 	}, "");
-	return <p className={styles.text}>{phrase}</p>;
+	return <p className={styles.text}>{text}</p>;
 };
