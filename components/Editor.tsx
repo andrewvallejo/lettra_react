14e3871b8d9834a29d650ca9fbe1c37ca9915482
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-09 14:51:00 -0600

    Remove unnecssary elements

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -5,14 +5,10 @@ import TextEditor from "./TextEditor";
 export default function Editor (){
 	return (
 		<section className={styles.container}>
-			<header>
-				<h1 className={styles.heading}>Lettra</h1>
-			</header>
-			<main className={styles.editor}>
-				<TextEditor>
-					<LiveEditor />
-				</TextEditor>
-			</main>
+			<h1 className={styles.heading}>Lettra</h1>
+			<TextEditor>
+				<LiveEditor />
+			</TextEditor>
 		</section>
 	);
 }
