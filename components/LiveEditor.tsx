1b7f17d3021e666f03b5456f304b953f42be2b25
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-09 15:29:56 -0600

    Move lgoic from LiveEditor to Editor

--- components/LiveEditor.tsx
+++ components/LiveEditor.tsx
@@ -1,27 +1,5 @@
-import { useEffect, useMemo, useState } from "react";
-
-import { UseWordiablesContext } from "../context/wordiablesContext";
-import { checkWordiableStatus, parseText } from "../lib/editor";
 import styles from "../styles/editor.module.scss";
 
-export default function LiveEditor (){
-	const { wordiablesContext, setWordiablesContext, textContext } = UseWordiablesContext();
-
-	useEffect(
-		() => {
-			const liveWordiables: string[] = [];
-			textContext.split(" ").forEach((word: string) => {
-				const isWordiable: boolean = checkWordiableStatus(word);
-				if (isWordiable && !liveWordiables.includes(word)) {
-					liveWordiables.push(word);
-				} else if (!isWordiable && liveWordiables.includes(word)) {
-					liveWordiables.splice(liveWordiables.indexOf(word), 1);
-				}
-				setWordiablesContext(liveWordiables);
-			});
-		},
-		[ textContext, wordiablesContext, setWordiablesContext ]
-	);
-
-	return <article className={styles.liveEditor}>{parseText(textContext, wordiablesContext)}</article>;
+export default function LiveEditor ({ children }: { children: any }){
+	return <article className={styles.liveEditor}>{children}</article>;
 }
