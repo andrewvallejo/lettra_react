80fbf3454fad7394dcd8896ca24c49d081354cf9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-13 15:23:57 -0600

    Add logic to add space around newlines

--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -1,12 +1,37 @@
 import "normalize.css/normalize.css";
 
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import { UseWordiablesContext } from "../context/wordiablesContext";
 import styles from "../styles/editor.module.scss";
 
 export default function TextEditor ({ children }: { children: React.ReactNode }){
-	const { textContext, setTextContext } = UseWordiablesContext();
+	const { textContext, setTextContext, setWordiablesContext, wordiablesContext } = UseWordiablesContext();
+	// const regex = /\\([A-Z])\w+\\/;
+
+	useEffect(
+		() => {
+			textContext.split(" ").forEach((word: string) => {
+				const regex = /\\([A-Z])\w+\\\n/gi;
+				const regex2 = /\n\\([A-Z])\w+\\/gi;
+				const match = regex.exec(word);
+				const match2 = regex2.exec(word);
+				if (match) {
+					// sp;loce the \n from the end of the word
+					const wordWithoutLastTwoCharacters = word.trim();
+					// add a space after the word with a \n
+					const wordWithSpace = `${wordWithoutLastTwoCharacters} \n`;
+					setTextContext(textContext.replace(word, wordWithSpace));
+				}
+				if (match2) {
+					const wordWithoutFirstTwoCharacters = word.trim();
+					const wordWithSpace = `\n ${wordWithoutFirstTwoCharacters}`;
+					setTextContext(textContext.replace(word, wordWithSpace));
+				}
+			});
+		},
+		[ setTextContext, textContext ]
+	);
 
 	return (
 		<article className={styles.editor}>
