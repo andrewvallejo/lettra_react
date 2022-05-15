195dbb14f8fbe59db13b9d9e4ecfb2d85444ae7b
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-15 12:23:19 -0600

    Fix regex

--- lib/editor.tsx
+++ lib/editor.tsx
@@ -1,11 +1,17 @@
 import styles from "../styles/editor.module.scss";
 
-export const wordiableRegex = /.?^\\.[^,.?!;:|\\n]*\\$/gi;
+export const wordiableRegex = /\B\\[^,.?!;:|\\n]*\\/gi;
 
 export const stringToJSX = (text: string, wordiables: string[]) => {
-	return text.split(" ").map((word, index) => {
-		const wordiable = word.match(wordiableRegex);
-		if (wordiable) {
+	return text.trim().split(" ").map((word, index) => {
+		if (word.match(/\n*/gi)) {
+		}
+
+		if (word === " ") {
+			return <span key={index}>&nbsp;</span>;
+		}
+
+		if (word.match(wordiableRegex)) {
 			return (
 				<span className={styles.one} key={index}>
 					{word + " "}
